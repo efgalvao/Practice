31ca06faee8f290ce1848237f04c750344ea92e4
efgalvao <Az123456!@#>
2020-03-20 16:34:40 -0300

    Done

--- Find Numbers with Even Number of Digits.py
+++ Find Numbers with Even Number of Digits.py
@@ -3,17 +3,23 @@ Given an array nums of integers, return how many of them contain an even number 
 
 Example 1:
 
-Input: nums = [12,345,2,6,7896]
-Output: 2
-
-Example 2:
-
-Input: nums = [555,901,482,1771]
-Output: 1
+    >>> nums = [12,345,2,6,7896]
+    >>> findNumbers(nums)
+    2
+    >>> nums = [555,901,482,1771]
+    >>> findNumbers(nums)
+    1
 
 """
-def findNumbers(self, nums):
+def findNumbers(nums):
     """
     :type nums: List[int]
     :rtype: int
     """
+    count = 0
+    for n in nums:
+        if len(str(n)) % 2 == 0:
+            count += 1
+
+    return count
+
