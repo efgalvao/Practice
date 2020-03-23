c859f7e9cfe972ff97c4da905d4c822aabcfd927
efgalvao <Az123456!@#>
2020-03-23 19:38:47 -0300

    Começando.

---
+++ Range Sum of BST.py
@@ -0,0 +1,46 @@
+"""
+Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+
+
+
+Example 1:
+
+Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+Output: 32
+
+Example 2:
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+Output: 23
+
+"""
+
+
+ Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def rangeSumBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: int
+        """
+        t = []
+        s = 0
+        while t:
+            t.add(str(t))
+            t.next
+
+        for i in t:
+            if i >= L or i <= R:
+                s += i
+
+        return s
