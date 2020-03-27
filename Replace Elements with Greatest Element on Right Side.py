38b157904a6098085767f744d7c19cbfa0a792d3
efgalvao <Az123456!@#>
2020-03-27 17:53:55 -0300

    Solved.

--- Replace Elements with Greatest Element on Right Side.py
+++ Replace Elements with Greatest Element on Right Side.py
@@ -11,4 +11,19 @@ Input: arr = [17,18,5,4,6,1]
 Output: [18,6,6,6,1,-1]
 
 """
+arr = [17, 18, 5, 4, 6, 1]
 
+
+def replaceElements(arr):
+    out = [-1]
+    l = len(arr) - 1
+    maior_direita = arr[-1]
+    for i in range(l)[::-1]:
+        out.append(maior_direita)
+        if arr[i] > maior_direita:
+            maior_direita = arr[i]
+
+
+    return out[::-1]
+
+print(replaceElements(arr))
