6743617aa2749fff2098910fa416303833ffe313
efgalvao <solfieri@hotmail.com>
2020-04-14 18:23:30 -0300

    É isso, retornar depois.

--- Two Sum.py
+++ Two Sum.py
@@ -3,12 +3,24 @@ Given an array of integers, return indices of the two numbers such that they add
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-Example:
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-
+>>> nums = [2, 7, 11, 15]
+>>> target = 9
+[0, 1]
+>>> nums = [3, 3]
+>>> target = 6
+[0, 1]
 
 """
+
+def twoSum(nums, target):
+    for i in nums:
+        ij = 0
+        for j in nums:
+            print(ij)
+            if i + j == target and nums.index(i) != ij:
+                return [nums.index(i), ij]
+            ij += 1
+
+nums = [3, 3]
+target = 6
+print(twoSum(nums, target))
