bef898dc9d54db9e3739450209663f2ddfc12ec7
efgalvao <solfieri@hotmail.com>
2020-04-16 14:23:43 -0300

    Pronto. Envolve listas encadeadas.

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -11,6 +11,11 @@ Example:
 [7, 0, 8]
 
 """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
     num1 = []
