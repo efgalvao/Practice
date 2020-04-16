648f0222cda72626819c1c44b67be6b22637e5db
efgalvao <solfieri@hotmail.com>
2020-04-16 14:21:55 -0300

    Começando mais um para praticar.

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -5,9 +5,38 @@ You may assume the two numbers do not contain any leading zero, except the numbe
 
 Example:
 
-Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 0 -> 8
-Explanation: 342 + 465 = 807.
-
+>>> l1[2, 4, 3]
+>>> l2[5, 6, 4]
+>>> addTwoNumbers(l1, l2)
+[7, 0, 8]
 
 """
+
+def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    num1 = []
+    while l1 != None:
+        num1.append(l1.val)
+        l1 = l1.next
+
+    num1 = num1[::-1]
+    num1 = int(''.join(str(i) for i in num1))
+
+    num2 = []
+    while l2 != None:
+        num2.append(l2.val)
+        l2 = l2.next
+
+    num2 = num2[::-1]
+    num2 = int(''.join(str(i) for i in num2))
+
+    soma = (str(num1 + num2))[::-1]
+
+    lista = ListNode(int(soma[0]))
+    out = lista
+
+    for i in range(1, len(soma)):
+        node = ListNode(int(soma[i]))
+        out.next = node
+        out = out.next
+
+    return lista
