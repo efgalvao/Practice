8ca7f98c0cbb6197b99d172c70c98690fef51c52
efgalvao <solfieri@hotmail.com>
2020-04-28 16:21:49 -0300

    WIP

---
+++ Primes.py
@@ -0,0 +1,22 @@
+"""
+Count the number of prime numbers less than a non-negative number, n.
+
+>>> n = 10
+>>> countPrimes(n)
+4
+"""
+
+def primos(n):
+    primos = 1
+    for i in range(3, n):
+        divisores = 2
+        if i % 2 == 0:
+            continue
+        for j in range(2, i//2 + 1):
+            if i % j == 0:
+                divisores += 1
+                break
+        if divisores == 2:
+            primos += 1
+    return primos
+
