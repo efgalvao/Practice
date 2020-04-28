ae2cf168dcd29330ea965315c7e8b56a0a22308f
efgalvao <solfieri@hotmail.com>
2020-04-28 19:40:36 -0300

    Feito

--- Self dividing numbers.py
+++ Self dividing numbers.py
@@ -8,5 +8,24 @@ Also, a self-dividing number is not allowed to contain the digit zero.
 Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
 """
 
+def selfDividingNumbers(left: int, right: int):
+    out = []
+    for i in range(left, right + 1):
+        if i < 10:
+            out.append(i)
+        else:
+            temp = str(i)
+            count = 0
+            for d in temp:
+                if int(d) == 0:
+                    break
+                elif i % int(d) != 0:
+                    break
+                elif i % int(d) == 0:
+                    count += 1
+            if count == len(temp):
+                out.append(i)
 
-def selfDividingNumbers(left: int, right: int)
+    return out
+
+print(selfDividingNumbers(1,10000))
