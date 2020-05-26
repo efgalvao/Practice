0378e1bc1280bd3ebdf60d8b39600ca0c2fa6b60
efgalvao <solfieri@hotmail.com>
2020-05-26 18:59:59 -0300

    New one.

---
+++ JavaScript/Escada.js
@@ -0,0 +1,27 @@
+/*
+Desafio - Escada
+
+Queremos representar uma escada com altura variável, utilizando um array de strings.
+
+Por exemplo, uma escada com altura 3, representaremos com o seguinte array:
+
+var escada3 = [
+ "  #",
+ " ##",
+ "###"
+]
+
+E uma escada com altura 5, da seguinte forma:
+
+var escada5 = [
+ "    #",
+ "   ##",
+ "  ###",
+ " ####",
+ "#####"
+]
+*/
+
+function escada(n){
+    
+}
