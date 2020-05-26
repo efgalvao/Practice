a5d542d31d5865ac63ff0c3136e05216fe135599
efgalvao <solfieri@hotmail.com>
2020-05-26 18:49:12 -0300

    Done.

--- JavaScript/Fatorial.js
+++ JavaScript/Fatorial.js
@@ -4,7 +4,9 @@ Escreva uma função fatorial, que calcule o fatorial de um número.
 
 function fatorial(n){
     var total = 1
-    for(i = n; i > 1; i-- ){
-        
-    }
+    for(var i = 1; i <= n; i++ ){
+        total = i * total
+    } return total
 }
+
+console.log(fatorial(5))
