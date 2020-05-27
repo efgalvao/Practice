43c21f09998b5828124858729a29a6f5bd36e991
efgalvao <solfieri@hotmail.com>
2020-05-27 16:58:31 -0300

    Baby steps.

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,5 +1,37 @@
+/*
 var Dog = require('./cachorro');
 
-console.log(Dog.nome)
-console.log(Dog.idade)
-Dog.latir()
+//console.log(Dog.nome)
+//console.log(Dog.idade)
+//Dog.latir()
+
+//-----------------------------------------//
+
+var EventEmitter = require('events')
+
+var emmiter = new EventEmitter();
+
+emmiter.on('meu_evento', function(number){;
+        console.log('Trying to learn Node.js', number )
+});
+
+emmiter.emit('meu_evento', 101)
+--------------------------------------------
+*/
+
+var EventEmitter = require('events')
+class Cao extends EventEmitter{
+    latir(){
+        console.log('Au Au')}}
+    
+var Rex = new Cao()
+
+Rex.on('Bell', Rex.latir);
+Rex.once('Bell2', Rex.latir);
+
+Rex.emit('Bell')
+Rex.emit('Bell2')
+Rex.emit('Bell2')
+Rex.removeListener('Bell', Rex.latir)
+Rex.emit('Bell')
+Rex.emit('Bel2l')
