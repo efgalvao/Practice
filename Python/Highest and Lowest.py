7dda4488a3313e65742126bee167400f6a08b7e8
efgalvao <solfieri@hotmail.com>
2020-06-02 09:50:42 -0300

    Done.

--- Python/Highest and Lowest.py
+++ Python/Highest and Lowest.py
@@ -1,3 +1,6 @@
 """
 In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 """
+def high_and_low(numbers):
+    numbers = list(map(int, numbers.split(' ')))
+    return str(f'{max(numbers)} {min(numbers)}')
