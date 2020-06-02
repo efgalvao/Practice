1eac42e29363eec821c69f151c1b28a28ce03dae
efgalvao <solfieri@hotmail.com>
2020-06-02 09:10:21 -0300

    Done.

--- Python/Square Every Digit.py
+++ Python/Square Every Digit.py
@@ -1,5 +1,12 @@
 """
 Welcome. In this kata, you are asked to square every digit of a number.
 
-For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+For example, if we run 9119 through the function, 811181 will come out, because 9**2 is 81 and 1**2 is 1.
 """
+def square_digits(num):
+    num = str(num)
+    out = str()
+    for n in num:
+        out += str((int(n)**2))
+    return int(out)
+
