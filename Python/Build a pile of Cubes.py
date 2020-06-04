60614aaf4c453ea1b9e3834c58b346ab680b5dc9
efgalvao <solfieri@hotmail.com>
2020-06-03 22:53:59 -0300

    Done.

--- Python/Build a pile of Cubes.py
+++ Python/Build a pile of Cubes.py
@@ -5,3 +5,22 @@ You are given the total volume m of the building. Being given m can you find the
 
 The parameter of the function findNb (find_nb, find-nb, findNb) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
 """
+def find_nb(m):
+    n = 0
+    x = 1
+    counter = 0
+    while n <= m:
+        n += x ** 3
+        x += 1
+        counter +=1
+        if n == m:
+            return counter
+        elif n > m:
+            return -1
+
+print(find_nb(91716553919377))
+print(find_nb(4183059834009))
+print(find_nb(24723578342962))
+print(find_nb(135440716410000))
+print(find_nb(40539911473216))
+print(find_nb(26825883955641))
