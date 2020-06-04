a22179343951c9f7093ae3602f18f8ed974af177
efgalvao <solfieri@hotmail.com>
2020-06-04 11:37:39 -0300

    2 to one done.

--- Python/Two to One.py
+++ Python/Two to One.py
@@ -1,5 +1,14 @@
 """
-Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters,
 
     each taken only once - coming from s1 or s2.
 """
+def longest(s1, s2):
+    longe = list(set(s1).union(set(s2)))
+    longe = sorted(longe)
+    longe ="".join(longe)
+    return longe
+
+print(longest("aretheyhere", "yestheyarehere")) #"aehrsty"
+print(longest("loopingisfunbutdangerous", "lessdangerousthancoding")) #"abcdefghilnoprstu")
+print(longest("inmanylanguages", "theresapairoffunctions")) #"acefghilmnoprstuy")
