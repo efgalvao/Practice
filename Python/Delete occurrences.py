e0402dd5c21e7ec29a598d7fe69fdac15a5d164b
efgalvao <solfieri@hotmail.com>
2020-06-05 14:14:42 -0300

    Delete occurrences Done.

--- Python/Delete occurrences.py
+++ Python/Delete occurrences.py
@@ -6,3 +6,15 @@ Task
 
 Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
 """
+def delete_nth(order,max_e):
+    out = []
+    for n in order:
+        if out.count(n) < max_e:
+            out.append(n)
+        else: 
+            continue
+    return out
+
+
+print(delete_nth([20,37,20,21], 1)) #[20,37,21])
+print(delete_nth([1,1,3,3,7,2,2,2,2], 3)) # [1, 1, 3, 3, 7, 2, 2, 2])
