af717a8d357bc8d05a953f35dcd1ce83f8624187
efgalvao <solfieri@hotmail.com>
2020-06-07 15:24:00 -0300

    Turn off the lights

---
+++ Python - 1/Turn Off the Lights.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+def turnOffTheLights(k, c):
+    x = 0
+    soma = 0
+    while x < (len(c) - 1):
+        soma += c[k]
+        x += k
+
+    return soma
+
+    print(turnOffTheLights(1, [1,1,1]))
