4b2bf1c9ae8af1a3ce59feb3243df3ed62acaa11
efgalvao <solfieri@hotmail.com>
2020-06-08 12:02:32 -0300

    Kangaroo finished.

--- Python - 1/Kangaroo.py
+++ Python - 1/Kangaroo.py
@@ -1,3 +1,10 @@
 """
 https://www.hackerrank.com/challenges/kangaroo/problem
 """
+def kangaroo(x1, v1, x2, v2):
+    for j in range(1, 10001):
+        k1 = x1 + (v1 * j)
+        k2 = x2 + (v2 * j)
+        if k1 == k2:
+            return 'YES'
+    return 'NO'
