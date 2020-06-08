17822dca820895f54adcdc2d41770433ccbb0750
efgalvao <solfieri@hotmail.com>
2020-06-08 11:49:18 -0300

    Apple and oranges done.

--- Python - 1/apple and orange.py
+++ Python - 1/apple and orange.py
@@ -1,3 +1,14 @@
 """
 https://www.hackerrank.com/challenges/apple-and-orange/problem
 """
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    ac = 0
+    oc = 0
+    for ap in apples:
+        if (ap + a) >= s and (ap + a) <= t:
+            ac +=1
+    for o in oranges:
+        if (o + b) >= s and (o + b) <= t:
+            oc +=1
+    return ac, oc
+
