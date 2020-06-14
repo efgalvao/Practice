111bb600024845f1af6ceecbdb74e793af093dc2
efgalvao <solfieri@hotmail.com>
2020-06-13 22:23:14 -0300

    Extra long factorial finished.

--- Python - 2/Extra long factorials.py
+++ Python - 2/Extra long factorials.py
@@ -2,3 +2,10 @@
 https://www.hackerrank.com/challenges/extra-long-factorials/problem
 """
 def extraLongFactorials(n):
+    out = 1
+    for n in range(1, n+1):
+        out *= n
+    return out
+
+
+print(extraLongFactorials(45))
