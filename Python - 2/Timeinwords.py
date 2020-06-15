d064f594ae0c63bdea59a0207c610d6282c3d8ce
efgalvao <solfieri@hotmail.com>
2020-06-14 22:15:47 -0300

    Time in words done.

--- Python - 2/Timeinwords.py
+++ Python - 2/Timeinwords.py
@@ -2,106 +2,30 @@
 https://www.hackerrank.com/challenges/the-time-in-words/problem
 """
 def timeInWords(h, m):
-numbers =1
-one 	2
-two 	3
-three 	4
-four 	5
-five 	6
-six 	7
-seven 	8
-eight 	9
-nine 	10
-ten
-11
-eleven 	12
-twelve 	13
-thirteen 	14
-fourteen 	15
-fifteen 	16
-sixteen 	17
-seventeen 	18
-eighteen 	19
-nineteen 	20
-twenty
-21
-twenty-
-one 	22
-twenty-
-two 	23
-twenty-
-three 	24
-twenty-
-four 	25
-twenty-
-five 	26
-twenty-
-six 	27
-twenty-
-seven 	28
-twenty-
-eight 	29
-twenty-
-nine 	30
-thirty
-31
-thirty-
-one 	32
-thirty-
-two 	33
-thirty-
-three 	34
-thirty-
-four 	35
-thirty-
-five 	36
-thirty-
-six 	37
-thirty-
-seven 	38
-thirty-
-eight 	39
-thirty-
-nine 	40
-forty
-41
-forty-
-one 	42
-forty-
-two 	43
-forty-
-three 	44
-forty-
-four 	45
-forty-
-five 	46
-forty-
-six 	47
-forty-
-seven 	48
-forty-
-eight 	49
-forty-
-nine 	50
-fifty
-51
-fifty-
-one 	52
-fifty-
-two 	53
-fifty-
-three 	54
-fifty-
-four 	55
-fifty-
-five 	56
-fifty-
-six 	57
-fifty-
-seven 	58
-fifty-
-eight 	59
-fifty-
-nine
-if m == 0:
-    print(f'')
+    numbers = {0:"o' clock", 1:'one',	2:'two', 3:'three', 4:'four', 5:'five',	6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten', 11:'eleven',	12:'twelve', 13:'thirteen',	14:'fourteen', 15:'quarter', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty', 21:'twenty one', 22:'twenty two',	23:'twenty three', 24:'twenty four', 25:'twenty five', 26:'twenty six', 27:'twenty seven', 28:'twenty eight', 29:'twenty nine', 30:'half'}
+    hour = numbers[h]
+    if (m < 15 and m > 1) or (m > 15 and m < 30):
+        minu = numbers[m]
+        return (f'{minu} minutes past {hour}')
+    elif m == 1:
+        minu = numbers[m]
+        return (f'{minu} minute past {hour}')
+    elif m == 15:
+        return (f'quarter past {hour}')
+    elif m == 30:
+        return(f'half past {hour}')
+    elif (m < 45 and m > 31) or (m > 45):
+        minu = numbers[abs(60 - m)]
+        hour = numbers[h + 1]
+        return (f'{minu} minutes to {hour}')
+    elif m == 45:
+        hour = numbers[h + 1]
+        return (f'quarter to {hour}')
+    elif m == 31:
+        minu = numbers[abs(60 - m)]
+        hour = numbers[h + 1]
+        return (f'{minu} minute to {hour}')
+    elif m == 0:
+        return (f'{hour} o\' clock')
+
+print(timeInWords(5, 45))
