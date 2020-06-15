7f43cc51cb8fc51db8c31c55fcca82df19240288
efgalvao <solfieri@hotmail.com>
2020-06-14 23:11:06 -0300

    Done.

--- Python - 1/Chocolate feast.py
+++ Python - 1/Chocolate feast.py
@@ -2,3 +2,14 @@
 https://www.hackerrank.com/challenges/chocolate-feast/problem?h_r=next-challenge&h_v=zen
 '''
 def chocolateFeast(n, c, m):
+    total = (n//c) 
+    emb = total
+    while emb >= m:
+        emb -= m
+        emb += 1
+        total += 1
+    return total
+   
+print(chocolateFeast(12951,19,5450))
+print(chocolateFeast(50045,193,47247))
+print(chocolateFeast(6,2,2))
