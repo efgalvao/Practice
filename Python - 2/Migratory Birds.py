d2feed75d29aafd9b34d05258f4fdcf56101e189
efgalvao <solfieri@hotmail.com>
2020-08-04 18:05:10 -0300

    Migratory Birds Initial

---
+++ Python - 2/Migratory Birds.py
@@ -0,0 +1,14 @@
+"""
+    https://www.hackerrank.com/challenges/migratory-birds/problem?h_r=next-challenge&h_v=zen
+"""
+
+def migratoryBirds(arr):
+    types = {1:0, 2:0, 3:0, 4:0, 5:0}
+    
+
+
+
+
+n= 6
+arr = [1, 4, 4, 4, 5, 3]
+print(migratoryBirds(arr))
