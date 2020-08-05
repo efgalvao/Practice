9b57b63ebb1ef95c81664ac3e3310be0e1a392f5
efgalvao <solfieri@hotmail.com>
2020-08-05 20:50:11 -0300

    Day of the programer ok.

--- Python - 2/Day of programmer.py
+++ Python - 2/Day of programmer.py
@@ -1,3 +1,19 @@
+import datetime 
 """
     https://www.hackerrank.com/challenges/day-of-the-programmer/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 """
+def dayOfProgrammer(year):
+    days = 255
+    if year < 1918 and year % 4 == 0:
+        days -= 1
+        if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+            days += 1
+    elif year == 1918:
+        days += 13
+    year = datetime.date(year, 1, 1)
+    data = year + datetime.timedelta(days)
+    data = data.strftime("%d.%m.%Y")
+    return(data)
+
+print(dayOfProgrammer(1812))
+# 1812 - 12/09
