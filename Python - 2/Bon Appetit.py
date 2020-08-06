5ac1d3fbbff6fed12a459fc55c7520afceaee42f
efgalvao <solfieri@hotmail.com>
2020-08-05 21:09:39 -0300

    Bon appetit ok.

--- Python - 2/Bon Appetit.py
+++ Python - 2/Bon Appetit.py
@@ -1,3 +1,17 @@
 """
     https://www.hackerrank.com/challenges/bon-appetit/
 """
+def bonAppetit(bill, k, b):
+    bill.pop(k)
+    total = sum(bill)
+    if total / 2 == b:
+        return "Bon Appetit"
+    else:
+        return int(b - total / 2 )
+
+
+k = 1
+bill = [3, 10, 2, 9]
+b = 12
+
+print(bonAppetit(bill, k, b))
