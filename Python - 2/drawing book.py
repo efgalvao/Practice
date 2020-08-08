8cb4b9f60a61c97b2137eea909f4e6ed61322213
efgalvao <solfieri@hotmail.com>
2020-08-08 00:26:52 -0300

    drawing book ok.

--- Python - 2/drawing book.py
+++ Python - 2/drawing book.py
@@ -2,3 +2,17 @@
     https://www.hackerrank.com/challenges/drawing-book/problem
 """
 def pageCount(n, p):
+    front = 0
+    back = 0
+    if p == 1 or p == n:
+        return 0
+    for x in range(1,p,2):
+        front += 1
+    for x in range(n, p, -2):
+        if x % 2 != 0 and x -1 == p:
+            break
+        else:
+            back += 1
+    return(min(front, back))
+
+print(pageCount(2059, 117))
