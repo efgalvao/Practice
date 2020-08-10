b749c002b4d10f74ab6bd5d77c0e8c422e341aa1
efgalvao <solfieri@hotmail.com>
2020-08-10 19:39:27 -0300

    counting valleys wip

--- Python - 2/counting valleys.py
+++ Python - 2/counting valleys.py
@@ -2,3 +2,19 @@
     https://www.hackerrank.com/challenges/counting-valleys/problem
 """
 def countingValleys(n, s):
+    valleys = 0
+    aux = 0
+    stri = [x for x in s]
+    for s in stri:
+        print(s)
+        if s == "D":
+            aux -= 1
+        else:
+            aux += 1
+        if aux == -1:
+            valleys += 1
+    return valleys
+
+n = 8
+s = "UDDDUDUU"
+print(countingValleys(n, s))
