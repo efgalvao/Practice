34d00326dd8367d2900d8bf213269baa7cf8907c
efgalvao <solfieri@hotmail.com>
2020-10-16 19:14:42 -0300

    rails course.

---
+++ Ruby/course/test/controllers/produtos_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class ProdutosControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get produtos_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get produtos_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get produtos_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get produtos_edit_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get produtos_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get produtos_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get produtos_destroy_url
+    assert_response :success
+  end
+
+end
